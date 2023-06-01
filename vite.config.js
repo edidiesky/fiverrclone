59f3a5901b4f3fc9a3e699878f8333ff40dea997
edidiesky <essienedidiong1000@gmail.com>
2023-06-01 01:17:55 -0700

    new fixes

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,5 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  server: {
-    proxy: {
-      "/api/v1": {
-        target: "http://localhost:5000/",
-      },
-    },
-  },
   plugins: [react()],
 });
