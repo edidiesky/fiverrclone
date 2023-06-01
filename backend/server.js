e9b0b255b4234c1127c0033e3e7547ee3047167a
edidiesky <essienedidiong1000@gmail.com>
2023-06-01 00:05:23 -0700

    new fixes

--- backend/server.js
+++ backend/server.js
@@ -63,10 +63,10 @@ mongoose.connect(
 // production mode process
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.join(__dirname, "/frontend/dist/index.html"))
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"))
   );
 } else { 
   app.get("/", (req, res) => {
