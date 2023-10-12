84514f986f7b85690af4f206a5950dd8f70f3477
edidiesky <essienedidiong1000@gmail.com>
2023-10-11 18:48:39 -0700

    new

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import replace from "@rollup/plugin-replace";
-console.log(VITE_API_BASE_URL_DEV);
 export default defineConfig({
   server: {
     proxy: {
       "/api/v1": {
-        target:'https://eddy-fiverr-clone.vercel.app/api/v1',
+        target: "https://eddy-fiverr-clone.vercel.app/api/v1",
       },
     },
   },
