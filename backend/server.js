46f4f8abb92dd2b2e4f2876f11ae37616c4678cd
edidiesky <essienedidiong1000@gmail.com>
2023-10-13 06:54:03 -0700

    new fixes

--- backend/server.js
+++ backend/server.js
@@ -68,7 +68,6 @@
 // });
 
 
-
 import express from "express";
 import path from "path";
 import dotenv from "dotenv";
@@ -77,26 +76,43 @@ const app = express();
 import { errorHandler, NotFound } from "./middleware/error-handler.js";
 
 import mongoose from "mongoose";
-// app.use(
-//   cors({
-//     origin: ["https://deploy-mern-frontend.vercel.app"],
-//     methods: ["POST", "GET"],
-//     credentials: true,
-//   })
-// );
+
+// middlewares
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// mongoose.connect(
-//   process.env.MONGO_URl,
-//   {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   },
-//   () => {
-//     console.log("mongo has been connected");
-//   }
-// );
+// routes
+import authRoute from "./routes/authRoute.js";
+import userRoute from "./routes/userRoute.js";
+import reviewRoute from "./routes/reviewRoutes.js";
+import gigRoute from "./routes/gigRoutes.js";
+import uploadRoute from "./routes/uploadRoute.js";
+import cartRoute from "./routes/cartRoute.js";
+import orderRoute from "./routes/orderRoutes.js";
 
+import chatRoute from "./routes/chatRoutes.js";
+
+app.use("/api/v1/auth", authRoute);
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/gig", gigRoute);
+app.use("/api/v1/review", reviewRoute);
+app.use("/api/v1/order", orderRoute);
+app.use("/api/v1/upload", uploadRoute);
+app.use("/api/v1/cart", cartRoute);
+
+mongoose.connect(
+  process.env.MONGO_URl,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  () => {
+    console.log("mongo has been connected");
+  }
+);
+
+// production mode process
+const __dirname = path.resolve();
 
 
 app.get("/", (req, res) => {
