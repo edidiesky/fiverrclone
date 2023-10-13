863a726676808c67c2b8eb5c915a3f036a9d90ec
edidiesky <essienedidiong1000@gmail.com>
2023-10-13 08:44:12 -0700

    new fixes

--- backend/server.js
+++ backend/server.js
@@ -14,8 +14,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // // // routes
-// import authRoute from "./routes/authRoute.js";
-// import userRoute from "./routes/userRoute.js";
+import authRoute from "./routes/authRoute.js";
+import userRoute from "./routes/userRoute.js";
 // import reviewRoute from "./routes/reviewRoutes.js";
 import gigRoute from "./routes/gigRoutes.js";
 // import uploadRoute from "./routes/uploadRoute.js";
@@ -24,8 +24,8 @@ import gigRoute from "./routes/gigRoutes.js";
 
 // import chatRoute from "./routes/chatRoutes.js";
 
-// app.use("/api/v1/auth", authRoute);
-// app.use("/api/v1/user", userRoute);
+app.use("/api/v1/auth", authRoute);
+app.use("/api/v1/user", userRoute);
 app.use("/api/v1/gig", gigRoute);
 // app.use("/api/v1/review", reviewRoute);
 // app.use("/api/v1/order", orderRoute);
