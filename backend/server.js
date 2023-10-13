1a6f6f996fde0dbdd5af4e1fd593690b066d0bf5
edidiesky <essienedidiong1000@gmail.com>
2023-10-13 07:11:41 -0700

    new fixes

--- backend/server.js
+++ backend/server.js
@@ -81,38 +81,38 @@ import mongoose from "mongoose";
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// routes
-import authRoute from "./routes/authRoute.js";
-import userRoute from "./routes/userRoute.js";
-import reviewRoute from "./routes/reviewRoutes.js";
-import gigRoute from "./routes/gigRoutes.js";
-import uploadRoute from "./routes/uploadRoute.js";
-import cartRoute from "./routes/cartRoute.js";
-import orderRoute from "./routes/orderRoutes.js";
+// // routes
+// import authRoute from "./routes/authRoute.js";
+// import userRoute from "./routes/userRoute.js";
+// import reviewRoute from "./routes/reviewRoutes.js";
+// import gigRoute from "./routes/gigRoutes.js";
+// import uploadRoute from "./routes/uploadRoute.js";
+// import cartRoute from "./routes/cartRoute.js";
+// import orderRoute from "./routes/orderRoutes.js";
 
-import chatRoute from "./routes/chatRoutes.js";
+// import chatRoute from "./routes/chatRoutes.js";
 
-app.use("/api/v1/auth", authRoute);
-app.use("/api/v1/user", userRoute);
-app.use("/api/v1/gig", gigRoute);
-app.use("/api/v1/review", reviewRoute);
-app.use("/api/v1/order", orderRoute);
-app.use("/api/v1/upload", uploadRoute);
-app.use("/api/v1/cart", cartRoute);
+// app.use("/api/v1/auth", authRoute);
+// app.use("/api/v1/user", userRoute);
+// app.use("/api/v1/gig", gigRoute);
+// app.use("/api/v1/review", reviewRoute);
+// app.use("/api/v1/order", orderRoute);
+// app.use("/api/v1/upload", uploadRoute);
+// app.use("/api/v1/cart", cartRoute);
 
-mongoose.connect(
-  process.env.MONGO_URl,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  },
-  () => {
-    console.log("mongo has been connected");
-  }
-);
+// mongoose.connect(
+//   process.env.MONGO_URl,
+//   {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   },
+//   () => {
+//     console.log("mongo has been connected");
+//   }
+// );
 
-// production mode process
-const __dirname = path.resolve();
+// // production mode process
+// const __dirname = path.resolve();
 
 
 app.get("/", (req, res) => {
@@ -124,3 +124,54 @@ app.use(errorHandler);
 app.listen(5000, () => {
   console.log("Server is Running");
 });
+
+
+
+
+// {
+//   "version": 2,
+//   "builds": [
+//     {
+//       "src": "server.js",
+//       "use": "@vercel/node"
+//     }
+//   ],
+//   "routes": [
+//     {
+//       "src": "/api/(.*)",
+//       "dest": "server.js"
+//     },
+//     {
+//       "src": "/api/v1/auth/(.*)",
+//       "dest": "server.js"
+//     },
+//     {
+//       "src": "/api/v1/user/(.*)",
+//       "dest": "server.js"
+//     },
+//     {
+//       "src": "/api/v1/gig/(.*)",
+//       "dest": "server.js"
+//     },
+//     {
+//       "src": "/api/v1/review/(.*)",
+//       "dest": "server.js"
+//     },
+//     {
+//       "src": "/api/v1/order/(.*)",
+//       "dest": "server.js"
+//     },
+//     {
+//       "src": "/api/v1/upload/(.*)",
+//       "dest": "server.js"
+//     },
+//     {
+//       "src": "/api/v1/cart/(.*)",
+//       "dest": "server.js"
+//     },
+//     {
+//       "src": "/(.*)",
+//       "dest": "server.js"
+//     }
+//   ]
+// }
