1eba96ecfa3a54710f17442a4afcaa2ba6171175
edidiesky <essienedidiong1000@gmail.com>
2023-10-15 19:36:31 -0700

    new

--- backend/server.js
+++ backend/server.js
@@ -54,10 +54,10 @@ mongoose.connect(
 
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../dist")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"))
+    res.sendFile(path.join(__dirname, "../dist/index.html"))
   );
 } else {
   app.get("/", (req, res) => {
