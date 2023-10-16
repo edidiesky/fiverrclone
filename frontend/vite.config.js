c3d52c527b7a03daca17f9942f4810ed7e54cb39
edidiesky <essienedidiong1000@gmail.com>
2023-10-15 20:04:17 -0700

    bew

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,14 +2,5 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  server: {
-    proxy: {
-      "/api/v1": {
-        target: "http://fiverrclone-api.vercel.app",
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
   plugins: [react()],
 });
